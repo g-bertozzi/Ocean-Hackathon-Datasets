42309c624d7ba127a871078361614c2e313ca2ee
Grace Bertozzi <grace.bertozzi@gmail.com>
2025-08-22 11:03:36 -0700

    feat: added provenance yaml

--- hackathon-datasets/boat-traffic/boat-traffic-data-fetcher.py
+++ hackathon-datasets/boat-traffic/boat-traffic-data-fetcher.py
@@ -5,13 +5,37 @@ Boat Traffic Challenge Datasets
  * A collection of still images taken every 5 minutes by a video camera positioned onshore near China Creek, Alberni Inlet.
  * A collection of still images taken every 5 minutes by a video camera positioned onshore near Cape Mudge in Discovery Passage, Campbell River. 
  * Possible addition: hydrophone recordings from underwater offshore from the Alberni Inlet location.
+
+Goal layout: 
+
+boat-traffic/
+    data/
+        CCSS/                 # 1 year of CCSS images (every 5 minutes)
+        <ONC_filename>.jpg
+        ...
+        CRSS/                 # 1 year of CRSS images (every 5 minutes)
+        <ONC_filename>.jpg
+        ...
+    metadata/
+        manifest.csv          # 4 columns: timestamp, locationCode, filename, path
+
+Size estimate: 28.5 GB
+- 105,120 files in 1 year (every 5 minutes)
+- Upper lim file size: 135606 bytes
+
+- Upper lim for challenge: 105120 x 135606 x 2 = 28,509,805,440 bytes = 28.5 GB 
+
 """
 
+import json
+import yaml
+import urllib
 import os, sys, argparse, hashlib, tempfile, shutil
 from pathlib import Path
 from datetime import datetime, timezone, timedelta
 import requests
 import time   
+import math
 import pandas as pd
 from dotenv import load_dotenv, find_dotenv
 import onc
@@ -27,11 +51,28 @@ MY_ONC = onc.ONC(TOKEN)
 CHINA_LOCATION = "CCSS"
 MUDGE_LOCATION = "CRSS"
 
-challenge_info = {
-    "boat-traffic": [CHINA_LOCATION , MUDGE_LOCATION],
+API_CALL_N = 0 # Count of API calls made
 
+PROV_INFO = {
+    "challenge": "boat-traffic",
+    "description": "1 year of still images (every 5 minutes) from shore station cameras at China Creek and Cape Mudge",
+    "api": {
+        # "call_{API_CALL_N}": {
+        #     "endpoint": "",
+        #     "parameters": {}, # locationCode, deviceCategoryCode, fileExtension, dateFrom, dateTo
+        #     "queryUrl": "",
+        #     "citation": "",
+        #     "doi": "",
+        # }
+        # "call_2": { ...
+    },
+    "manifest": {
+        "manifest_path": "",
+        "manefist_schema": [], # ['timestamp', 'locationCode', 'filename', 'path']
+    }
 }
 
+# Functions
 def calculate_difference(date1: str, date2: str) -> int:
     """
     Calculates the difference in days between two dates in ISO 8601 format.
@@ -48,11 +89,12 @@ def calculate_difference(date1: str, date2: str) -> int:
     # Calculate the duration in hours (there are 3600 seconds in an hour)
     return (end_time - start_time).days
 
-
-
 def get_6_month_filenames(locationCode: str, dateFrom: str, dateTo: str) -> list:
     """
     Returns a list of filenames for still images from the video camera at the specified location within a 6 month period.
+
+    Inputs:
+    Output:
     """
 
     params = {
@@ -61,18 +103,39 @@ def get_6_month_filenames(locationCode: str, dateFrom: str, dateTo: str) -> list
     'dateTo': dateTo,
     'deviceCategoryCode': "VIDEOCAM",
     'fileExtension': "jpg", # 'jpg' for still images
-    # 'rowLimit': 18, # max 18 
+    # 'returnOptions': "all" # NOTE: debugging to find file size demands
     }
 
     response = MY_ONC.getArchivefileByLocation(params) # Make request 
     files = response.get('files', []) # Isolate list of files
     n = len(files) # Number of files
 
+    # Isolate info for provenance - NOTE: done for each API call
+    query_url = response.get('queryUrl')
+    citations_info = response.get('citations')[0]
+    url_parsed = urllib.parse.urlparse(query_url) # Parse the URL and break into components
+
+    params_minus_token = params.copy()
+    del params_minus_token['token']
+    
+    # Update global PROV_INFO with API call details
+    global API_CALL_N, PROV_INFO
+
+    PROV_INFO["api"][f"call_{API_CALL_N}"] = {
+    "endpoint": url_parsed.path,
+    "parameters": params_minus_token,
+    "queryUrl": query_url,
+    "citation": citations_info['citation'],
+    "doi": citations_info['doi'],
+    }
+
+    API_CALL_N += 1 # Update global API call count
+
     # NOTE: debugging output
-    print("6 months of data")
-    print(f"file 1: {files[0] if files else 'No files found.'}")
-    print(f"file {n}: {files[-1] if files else 'No files found.'}")
-    print(f"num files: {n}")
+    # print("6 months of data")
+    # print(f"file 1: {files[0] if files else 'No files found.'}")
+    # print(f"file {n}: {files[-1] if files else 'No files found.'}")
+    # print(f"num files: {n}")
 
     return files
 
@@ -110,20 +173,6 @@ def filenames_to_manifest(filenames: list[str], locationCode: str, challenge: st
     - 'locationCode': the location code of the file
     - 'filename': the name of the file
     - 'path': planned local download path for the file
-
-    Goal: 
-
-    boat-traffic/
-        data/
-            CCSS/                 # all CCSS images in one folder (1 year max)
-            <ONC_filename>.jpg
-            ...
-            CRSS/
-            <ONC_filename>.jpg
-            ...
-        metadata/
-            manifest.csv          # 4 columns: timestamp, locationCode, filename, path
-
     """
    # Create list of dictionaries: list is dataframe, each dict is a row
     manifest_list = []
@@ -150,15 +199,26 @@ def filenames_to_manifest(filenames: list[str], locationCode: str, challenge: st
     os.makedirs(metadata_path.parent, exist_ok=True) # Ensure the parent directory exists
     df.to_csv(metadata_path, index=False) # Save CSV
 
+    # Isolate info for provenance - NOTE: DONE ONLY ONCE for all API calls together
+    global PROV_INFO
+    PROV_INFO["manifest"] = {
+        "manifest_path": str(metadata_path),
+        "manifest_schema": ['timestamp', 'locationCode', 'filename', 'path'],
+    }
+
     return df
 
-
-def download_from_manifest(manifest_df: pd.DataFrame, data_root: str = "./data", subsample: int | None = None, per_file_sleep: float = 0.05, retries: int = 3) -> None:
+def download_from_manifest(manifest_df: pd.DataFrame, data_root: str = "./data", subsample: int | None = None, per_file_sleep: float = 0.05) -> None:
     """
     Download files listed in `manifest_df` (must have ['filename','path'] columns).
 
     subsample:
       - If given, only download the first `subsample` rows (keeps time order).
+
+    Inputs:
+
+    Output:
+
     """
     df = manifest_df.head(subsample) if subsample else manifest_df
 
@@ -204,32 +264,66 @@ def download_from_manifest(manifest_df: pd.DataFrame, data_root: str = "./data",
             print(f"[progress] ok={ok} skipped={skipped} failed={failed}")
 
     # Delte temporary 'output' directory if it exists
-    if src.exists():
-        shutil.rmtree(src)
+    output_dir = Path("output")
+    if output_dir.exists():
+        shutil.rmtree(output_dir)
 
+    # Print summary
     summary = {"ok": ok, "skipped": skipped, "failed": failed}
     print(f"[done] {summary}")
     
-    return summary
+    return
 
+def make_prov(metadata_root: str = "./metadata"):
+    """
+    Save provenance info dictionary to a YAML file.
+    """
+    output_path = Path(metadata_root) / "provenance.yaml"
+    output_path.parent.mkdir(parents=True, exist_ok=True)
 
+    with open(output_path, "w") as f:
+        yaml.dump(PROV_INFO, f, sort_keys=False, default_flow_style=False)
+    print(f"[saved provenance] {output_path}") # NOTE: debug output
+
+    
 def main():
-    # get filenames
-    china_files = get_yr_filenames(locationCode = CHINA_LOCATION, dateFrom = "2023-09-01T00:00:00.000Z", dateTo = "2024-09-01T00:00:00.000Z")
+    yr_start = "2023-09-01T00:00:00.000Z"
+    yr_end = "2024-09-01T00:00:00.000Z"
 
-    # subsampled test files
+    # get filenames
+    china_files = get_yr_filenames(locationCode = CHINA_LOCATION, dateFrom = yr_start, dateTo = yr_end)
+    mudge_files = get_yr_filenames(locationCode = MUDGE_LOCATION, dateFrom = yr_start, dateTo = yr_end)
+
+    # Small test files
     small_test_files = china_files[:5]
 
-    import math
-    step = math.ceil(len(china_files) / 200)  # 200 evenly spaced samples
-    medium_test_files = china_files[::step]
+    # S/M test files
+    sm_step = math.ceil(len(china_files) / 200)  
+    sm_test_files = china_files[::sm_step]
 
+    # Medium test files
+    m_step = math.ceil(len(china_files) / 1000)  
+    medium_test_files = china_files[::m_step]
 
-    # build directory structure?
-    manifest_df = filenames_to_manifest(filenames = medium_test_files, locationCode = CHINA_LOCATION, challenge = "boat-traffic")
+    # Large test files
+    l_step = math.ceil(len(china_files) / 10000)  
+    large_test_files = china_files[::l_step]
+
+    # TODO: fix manifest so that it appends for multiple locations
+    # Build manifest
+    china_manifest_df = filenames_to_manifest(filenames = china_files, locationCode = CHINA_LOCATION, challenge = "boat-traffic")
+    mudge_manifest_df = filenames_to_manifest(filenames = mudge_files, locationCode = MUDGE_LOCATION, challenge = "boat-traffic")
+
+    # Build prov
+    make_prov()
+ 
+    # print(PROV_INFO)
 
     # store files?
-    download_from_manifest(manifest_df = manifest_df, subsample = 10)
+    # download_from_manifest(manifest_df = china_manifest_df, subsample= 10)
+    # download_from_manifest(manifest_df = mudge_manifest_df, subsample= 10)
+
+
 
 if __name__ == "__main__":
     main()
